58001b9a7f6c970f18ecdeb3aacccc510017eb87
pescarualex <pescarualexflorin@gmail.com>
2024-12-08 09:36:39 +0200

    developing site structure and initialize products object

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+const addToCartBtn = document.querySelector(".add-to-cart");
+
+const products = {
+    product1: {
+        name: "Asus laptop",
+        price: "$799",
+        description: "A 15.6 inch laptop with 8GB RAM and 256GB SSD"
+    },
+    product2: {
+        name: "Phone",
+        price: "$299",
+        description: "A smartphone with 4GB RAM and 64GB storage"
+    },
+    product3: {
+        name: "Watch",
+        price: "$199",
+        description: "A watch with 40mm LED screen"
+    },
+    product4: {
+        name: "T-shirt",
+        price: "$19.99",
+        description: "A t-shirt with a red and white striped sleeve"
+    },
+    product5: {
+        name: "Lenovo laptop",
+        price: "$1299",
+        description: "A 15.6 inch laptop with 32GB RAM and 512GB SSD"
+    },
+    product6: {
+        name: "Samsung phone",
+        price: "$199",
+        description: "A smartphone with 4GB RAM and 64GB storage"
+    },
+    product7: {
+        name: "Nike shoes",
+        price: "$199",
+        description: "A smartphone with 4GB RAM and 64GB storage"
+    }
+};
+
+
